ef93ace906e2178ddf1080c4367062236d89d35f
ParkChulkyu <toy__park@naver.com>
2020-01-10 13:21:53 +0900

    보낸다 동철

--- dongchul/src/com/dto/MemberDto.java
+++ dongchul/src/com/dto/MemberDto.java
@@ -2,11 +2,13 @@ package com.dto;
 
 public class MemberDto {
 	private String chul;
+	private String dong;
 	
 	public MemberDto() {}
 	
-	public MemberDto(String chul) {
+	public MemberDto(String chul, String dong) {
 		this.chul=chul;
+		this.dong=dong;
 	}
 
 }
